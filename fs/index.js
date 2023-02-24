e0d008ad0625e46374a0a6410f308b90de203d22
Akshar Jikadara <jikadara2aj@gmail.com>
2023-02-24 22:31:46 +0530

    Time stamp

--- fs/index.js
+++ fs/index.js
@@ -6,12 +6,18 @@ const fileDataBySunc = fs.readFileSync('ChatGPT.txt', 'utf-8')
 console.log(fileDataBySunc);
 
 // Asynchronous function
-const fileDataByAsync = fs.readFile('ChatGPT.txt', 'utf-8' ,(err, data) => {
-    console.log(data);
+fs.readFile('ChatGPT.txt', 'utf-8' ,(err, fileDataByAsync) => {
+    if(!err){
+        console.log(fileDataByAsync);
+    }
 })
+const timeStamp1 = performance.now()
 
 // function
 const addNumbers = (num1, num2) => {
     return num1+num2
 }
 console.log(addNumbers(10,30));
+const timeStamp2 = performance.now()
+
+console.log(timeStamp2 - timeStamp1);
